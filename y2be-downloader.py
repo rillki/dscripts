bff5b844dc127c2bbb848a37aeb0b338c4755c40
kirill.saidov <kirill.saidov.d@gmail.com>
2022-02-21 19:42:24 +0600

    reading urls saved to file functionality added

--- y2be-downloader.py
+++ y2be-downloader.py
@@ -44,28 +44,52 @@ def downloadFromPlaylist(url = None, audio = False, savePath = None, num = 0, ve
                 vid.streams.filter(Progressive = True, file_extension =
                         'mp4').order_by('resolution').desc().first().download(savePath)
 
+def downloadFromFile(fileName = None, audio = False, savePath = None, verbose = False):
+    if fileName is None:
+        print('No filename with URLs supplied. Exiting...')
+        return
+
+    # list of URLs
+    urls = list()
+
+    # parse files
+    with open(fileName, 'r') as file:
+        # by line
+        for line in file:
+            url = line.rstrip()
+
+            # skip empty lines
+            if url != '':
+                urls.append(url)
+
+    for i in urls:
+        downloadVideo(i, audio, savePath, verbose)
+
 # set up argparser
 parser = argparse.ArgumentParser('Youtube downloader v.1.0')
 parser.add_argument('-a', '--audio', metavar = '\b', action=argparse.BooleanOptionalAction, default = False, help = 'audio only')
 parser.add_argument('-n', '--quantity', metavar = '\b', type=int, default = 0, help = 'number of videos to download from a playlist')
 parser.add_argument('-u', '--url', metavar = '\b', default = None, help = 'youtube playlist or video url')
 parser.add_argument('-s', '--save', metavar = '\b', default ='y2b-downloads', help = 'save path')
+parser.add_argument('-f', '--file', metavar = '\b', default = None, help = 'file with youtube URLs (one per line)')
 parser.add_argument('-v', '--verbose', metavar = '\b', action=argparse.BooleanOptionalAction,
         default = False, help = 'verbose output')
 
 # parse args
 args = parser.parse_args()
 
-if args.url is None:
-    print('No URL supplied! Exiting...')
-    sys.exit()
-
 print("\n------------- PYTUBE DOWNLOADER -------------\n")
 
-if args.quantity > 0:
-    downloadFromPlaylist(args.url, args.audio, args.save, args.quantity, args.verbose)
+if args.file is None:
+    if args.url is None:
+        print('No URLs supplied! Exiting...')
+    else:
+        if args.quantity > 0:
+            downloadFromPlaylist(args.url, args.audio, args.save, args.quantity, args.verbose)
+        else:
+            downloadVideo(args.url, args.audio, args.save, args.verbose)
 else:
-    downloadVideo(args.url, args.audio, args.save, args.verbose)
+    downloadFromFile(args.file, args.audio, args.save, args.verbose)
 
 print('\n------------------- DONE --------------------\n')
 
