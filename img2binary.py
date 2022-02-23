699e1ebe627a33b5f65b77445aa6cf0a5662a7d8
Ki Rill <rill.ki@yahoo.com>
2022-02-23 12:17:13 +0600

    img to binary img converter

---
+++ img2binary.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import os
+import sys
+import cv2 as cv
+import numpy as np
+
+# default values
+imgs_folder = ''
+output_folder = ''
+
+# get arguments
+argv = sys.argv[1:]
+if len(argv) < 2:
+    print("\n#imgresize: imgresize.py [imgs_folder] [output_folder]\n")
+    sys.exit()
+else:
+    imgs_folder = argv[0]
+    output_folder = argv[1]
+
+# find all images
+imgs = [i for i in os.listdir(os.path.join(".", imgs_folder)) if '.jpg' in i or '.jpeg' in i or '.JPEG' in i or '.JPG' in i]
+
+for i in imgs:
+    # open image
+    img = cv.imread(os.path.join('.', imgs_folder, i), cv.IMREAD_GRAYSCALE)
+
+    # convert to binary
+    thresh, img = cv.threshold(img, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+
+    # save image
+    cv.imwrite(os.path.join('.', output_folder, i), img)
+
+    # verbose output
+    print(f"#imgresize: <{i}> converted!")
+
+
+
+
+
+
+
