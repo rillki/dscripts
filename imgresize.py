0fa039021aef06bd304234022008e565c0a5f6de
Ki Rill <rill.ki@yahoo.com>
2022-07-31 12:58:32 +0600

    imgresize up/downscaling added

--- imgresize.py
+++ imgresize.py
@@ -8,11 +8,13 @@ imgs_folder = ''
 output_folder = ''
 width = 640
 height = 640
+inter = cv.INTER_AREA
 
 # get arguments
 argv = sys.argv[1:]
 if len(argv) < 2:
-    print("\n#imgresize: imgresize.py [imgs_folder] [output_folder] {width} {height}\n")
+    print("\n#imgresize: imgresize.py [imgs_folder] [output_folder] {width} {height} up/down\n")
+    print("\n#imgresize: imgresize.py ../data ../data/tmp 1280 1080 up\n")
     sys.exit()
 elif len(argv) == 2:
     imgs_folder = argv[0]
@@ -21,11 +23,17 @@ elif len(argv) == 3:
     imgs_folder = argv[0]
     output_folder = argv[1]
     width = argv[2]
+elif len(argv) == 4:
+    imgs_folder = argv[0]
+    output_folder = argv[1]
+    width = argv[2]
+    height = argv[3]
 else:
     imgs_folder = argv[0]
     output_folder = argv[1]
     width = argv[2]
     height = argv[3]
+    inter = cv.INTER_LINEAR if argv[4] == 'up' else inter
 
 # find all images
 imgs = [i for i in os.listdir(os.path.join(".", imgs_folder)) if '.jpg' in i or '.jpeg' in i or '.JPEG' in i or '.JPG' in i]
@@ -36,7 +44,7 @@ for i in imgs:
     img = cv.imread(os.path.join(".", imgs_folder, i))
 
     # resize image
-    img = cv.resize(img, newSize)
+    img = cv.resize(img, newSize, interpolation = inter)
 
     # save image
     cv.imwrite(os.path.join(".", output_folder, i), img)
