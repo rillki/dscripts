7f000825b701ce449039abadbd200be117163ec1
Ki Rill <rill.ki@yahoo.com>
2024-04-02 15:42:32 +0500

    update

--- img_metadata.py
+++ img_metadata.py
@@ -1,21 +1,39 @@
-from PIL import Image
-from PIL.ExifTags import TAGS
+# module img_metadata
 
-# open the image
-image = Image.open("img.jpg")
+from exif import Image
 
-# extracting the exif metadata
-exifdata = image.getexif()
 
-# looping through all the tags present in exifdata
-for tagid in exifdata:
-	
-	# getting the tag name instead of tag id
-	tagname = TAGS.get(tagid, tagid)
+if __name__ == '__main__':
+    image = None
+    with open("img2.jpg", "rb") as file:
+        image = Image(file)
 
-	# passing the tagid to get its respective value
-	value = exifdata.get(tagid)
+    # check if image contains metadata
+    if image.has_exif:
+        """
+        ['<unknown EXIF tag 50341>', '_exif_ifd_pointer', '_interoperability_ifd_Pointer', '_segments', 
+        'brightness_value', 'color_space', 'components_configuration', 'compressed_bits_per_pixel', 
+        'compression', 'contrast', 'custom_rendered', 'datetime', 'datetime_digitized', 'datetime_original', 
+        'delete', 'delete_all', 'digital_zoom_ratio', 'exif_version', 'exposure_bias_value', 'exposure_mode', 
+        'exposure_program', 'exposure_time', 'f_number', 'file_source', 'flash', 'flashpix_version', 'focal_length', 
+        'focal_length_in_35mm_film', 'get', 'get_all', 'get_file', 'get_thumbnail', 'has_exif', 'image_description', 
+        'jpeg_interchange_format', 'jpeg_interchange_format_length', 'lens_model', 'lens_specification', 'light_source', 
+        'list_all', 'make', 'maker_note', 'max_aperture_value', 'metering_mode', 'model', 'offset_time', 
+        'offset_time_digitized', 'offset_time_original', 'orientation', 'photographic_sensitivity', 'pixel_x_dimension', 
+        'pixel_y_dimension', 'recommended_exposure_index', 'resolution_unit', 'saturation', 'scene_capture_type', 'scene_type', 
+        'sensitivity_type', 'sharpness', 'software', 'user_comment', 'white_balance', 'x_resolution', 'y_and_c_positioning', 
+        'y_resolution']
+        """
+        print(f'{image.make}')
+        print(f'{image.model}')
+        print(f'f{image.f_number}')
+        if image.exposure_time >= 1:
+            print(f'{image.exposure_time} sec')
+        else:
+            print(f'1/{1/image.exposure_time} sec')
+        print(f'{image.focal_length} mm')
+        print(f'ISO {image.photographic_sensitivity}')
 
-	# printing the final result
-	print(f"{tagname:25}: {value}")
+        # for key, value in image.get_all().items():
+        #     print(f'{key:25s}\t{value}')
 
--- insta_post_photo.py
+++ insta_post_photo.py
@@ -1,41 +1,34 @@
+# module insta_post_photo
+
 import os
 import sys
+import time
 import shutil
 import argparse
-from instabot import Bot
-from PIL import Image
-from PIL.ExifTags import TAGS
+from instagrapi import Client as InstaClient
 
-if os.path.exists('./config'):
-    shutil.rmtree('./config', ignore_errors=True)
+
+if __name__ == '__main__':
+    bot = InstaClient()
+    bot.login(username='username', password='password')
+    time.sleep(0.1)
+    bot.photo_upload(path='img.jpg', caption='api test')
+    time.sleep(0.1)
+    bot.logout()
+
 
 # set up args parser
-parser = argparse.ArgumentParser('Instagram Photo Uploader v.1.0')
-parser.add_argument('-u', '--username', metavar = '\b', type=str, help = 'your username')
-parser.add_argument('-p', '--password', metavar = '\b', type=str, help = 'your password')
-parser.add_argument('-i', '--image', metavar = '\b', type=str, help = 'path to image')
-parser.add_argument('-v', '--verbose', metavar = '\b', action=argparse.BooleanOptionalAction, default = True, help = 'verbose output')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser('Instagram Photo Uploader v.1.0')
+# parser.add_argument('-u', '--username', metavar = '\b', type=str, help = 'your username')
+# parser.add_argument('-p', '--password', metavar = '\b', type=str, help = 'your password')
+# parser.add_argument('-i', '--image', metavar = '\b', type=str, help = 'path to image')
+# parser.add_argument('-v', '--verbose', metavar = '\b', action=argparse.BooleanOptionalAction, default = True, help = 'verbose output')
+# args = parser.parse_args()
 
-# don't do anything if image does not exist
-if not os.path.exists(args.image):
-    print(f'<{args.image}> does not exist!')
-    sys.exit()
-
-# get image info
-img = Image.open(args.image)
-exif_data = img.getexif()
-
-for tagid in exif_data:
-    tag = TAGS.get(tagid, tagid)
-    data = exif_data.get(tagid)
-
-    if isinstance(data, bytes):
-        data = data.decode()
-
-    print(f'{tag:16}: {data}')
-
-# print(img)
+# # don't do anything if image does not exist
+# if not os.path.exists(args.image):
+#     print(f'<{args.image}> does not exist!')
+#     sys.exit()
 
 # bot = Bot()
 # bot.login(username=args.username, password=args.password)
